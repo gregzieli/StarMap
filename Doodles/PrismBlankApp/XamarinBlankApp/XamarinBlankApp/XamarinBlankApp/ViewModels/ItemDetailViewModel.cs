0b046ab1ae6fb999c5b7bd3c1d9d99457b972bd4
Grzegorz Zieliński <grzegorz.zielinski@accenture.com>
2019-04-24 16:03:36 +0200

    Added some templates to test

---
+++ Doodles/PrismBlankApp/XamarinBlankApp/XamarinBlankApp/XamarinBlankApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinBlankApp.Models;
+
+namespace XamarinBlankApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
